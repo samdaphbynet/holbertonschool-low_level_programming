a1a3135a958c8686f97aa6bb57bd08b70e798ea7
samdaphbynet <lj8@hotmail.fr>
2023-02-20 18:09:46 +0000

    program that prints all single digit numbers of base 10, can only use the putchar function

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+		while (i < 10)
+		{
+			if (i < 10)
+			{
+				putchar (i);
+			}
+			i++;
+		}
+		putchar ('\n');
+
+		return (0);
+}
