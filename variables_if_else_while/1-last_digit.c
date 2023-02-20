04a9663635c0af5955214c553a5d2cac2488c3dd
samdaphbynet <lj8@hotmail.fr>
2023-02-20 12:16:41 +0000

    random number to the variable (n) each time it is executed

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -15,7 +15,8 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	lastDigit = n % 10;	
+	lastDigit = n % 10;
+
 	if (lastDigit > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
@@ -30,5 +31,6 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
 	}
+
 	return (0);
 }
