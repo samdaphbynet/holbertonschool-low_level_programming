5ee82fbd0f2f4f99eeddd76eab7545e007ea3435
samdaphbynet <lj8@hotmail.fr>
2023-02-21 11:29:37 +0000

    program that prints _putchar, followed by a new line.

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'random number to the variable n each time it is executed'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
