9f73a62e949abdd8f550dc8c87ca510c23b31166
samdaphbynet <lj8@hotmail.fr>
2023-02-21 22:29:53 +0000

    function that checks for lowercase character, Prototype: int _islower(int c);

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,14 +2,14 @@
 
 /**
  * _islower(char c) -  function that checks for lowercase character.
- *
- * islower: Returns 1 if c is lowercase or Returns 0 otherwise 
+ * @c: single letter input
+ * islower: Returns 1 if c is lowercase or Returns 0 otherwise
  */
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
-        {
-                return (1);
-        }
-        return (0);
+	{
+		return (1);
+	}
+	return (0);
 }
