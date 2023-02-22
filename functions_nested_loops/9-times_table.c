baa4e86e4586cc44ff939b7de220e1770d36d7b1
samdaphbynet <lj8@hotmail.fr>
2023-02-22 19:08:04 +0000

    function that prints the 9 times table, starting with 0

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign -  function that prints the sign of a number.
+ * @n:  sign of a number input
+ * Return: Returns 1 or 0 or -1 if n>0 or n==0 or n<0
+ */
+
+void times_table(void)
+{
+	int i, j;
+
+		for (i = 0; i <= 9; i++)
+		{
+			for (j = 0; j < 10; j++)
+			{
+				_putchar(i*j);
+
+				if (j < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+			}
+		}
+}
