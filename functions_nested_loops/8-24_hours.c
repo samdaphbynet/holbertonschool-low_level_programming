5e9748bfd27131205574e790647b0deba8c30cd9
samdaphbynet <lj8@hotmail.fr>
2023-02-22 16:14:23 +0000

    function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -18,7 +18,7 @@ void jack_bauer(void)
 			_putchar((hour % 10) + '0');
 			_putchar(':');
 			_putchar((min / 10) + '0');
-			_putchar((hour % 10) + '0');
+			_putchar((min % 10) + '0');
 			_putchar('\n');
 		}
 	}
