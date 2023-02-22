68b2a7cb80cbf4b4de9a07a0a23c56680184b05a
samdaphbynet <lj8@hotmail.fr>
2023-02-22 22:37:31 +0000

    function that adds two integers and returns the result

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower -  function that checks for lowercase character.
+ * @c: single letter input
+ * Return: Returns 1 if c is lowercase or Returns 0 otherwise
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
