07411c601e71b07fcb0aad48a09808432d299691
samdaphbynet <lj8@hotmail.fr>
2023-02-23 14:59:01 +0000

    function that prints the numbers, from 0 to 9.

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -2,8 +2,7 @@
 
 /**
  * print_numbers -  function that prints the numbers, from 0 to 9.
- *@i: counter for bocle while.
- *Return: numbers, from 0 to 9.
+ * Return: numbers, from 0 to 9.
  */
 
 void print_numbers(void)
