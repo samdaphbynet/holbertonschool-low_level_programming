9dd839d22d5fee52e76c2a58e10859ccfd4ae421
samdaphbynet <lj8@hotmail.fr>
2023-02-24 20:04:31 +0000

    program that prints the numbers from 1 to 100, The “Fizz-Buzz test”

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: The “Fizz-Buzz test”
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if ((i > 0) && (i < 100))
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
