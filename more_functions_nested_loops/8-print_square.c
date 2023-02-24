36fbd860e9ea12825592bca8801b67d848dfeed3
samdaphbynet <lj8@hotmail.fr>
2023-02-24 18:42:13 +0000

    function that prints a square.

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -8,16 +8,18 @@
 
 void print_square(int size)
 {
-	int i;
+	int i, j;
 
-	for (i = 1; i <= size; i++)
+	for (i = 0; i < size; i++)
 	{
-		if (size > 0)
+		for (j = 0; j < size; j++)
 		{
 			_putchar('#');
+
+			if (size == 0)
+				_putchar('\n');
 		}
-		else if (size == 0)
-			_putchar('\n');
+		_putchar('\n');
 	}
 	_putchar('\n');
 }
