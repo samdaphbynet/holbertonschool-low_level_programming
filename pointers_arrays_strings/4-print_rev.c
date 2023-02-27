f6729c97c98e9c6e93a84545cbdb3e52f858c881
samdaphbynet <lj8@hotmail.fr>
2023-02-27 14:25:59 +0000

    function that prints a string, in reverse.

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -11,12 +11,6 @@ void print_rev(char *s)
 	int leng = strlen(s);
 	int g;
 
-	if (leng != '\0')
-	{
-		for (g = leng - 1; g >= 0; g--)
-			printf("%c", s[g]);
-	}
-
-	else
-		printf("\n");
+	for (g = leng - 1; g >= 0; g--)
+		printf("%c", s[g]);
 }
