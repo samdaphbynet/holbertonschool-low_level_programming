d4e815e6f58e4d97fab4bf08a76b1b0bc22c49a0
samdaphbynet <lj8@hotmail.fr>
2023-02-27 13:07:26 +0000

    function that prints a string, in reverse.

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - function that prints a string, in reverse.
+ * @s: string in reverse.
+ * Return: 0.
+ */
+
+void print_rev(char *s)
+{
+	int leng = strlen(s);
+	int g;
+
+	for (g = leng - 1; g >= 0; g--)
+	{
+		printf("%c", s[g]);
+	}
+}
