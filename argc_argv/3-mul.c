5536353672e3e2782b04b08edf7f525a19da1ff2
samdaphbynet <lj8@hotmail.fr>
2023-03-07 22:40:36 +0000

    program that multiplies two numbers.

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that multiplies two numbers.
+ * @argc: gives the number of command line items.
+ * @argv: contains the name of the command.
+ * Return: 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int res = 1;
+
+	if (argc != 3)
+		printf("Error");
+
+	for (i = 1; i < argc; i++)
+	{
+		res = res * strtol(argv[i], NULL, 10);
+	}
+	printf("%d", res);
+
+	return (1);
+}
