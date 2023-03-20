e769a69fffafefc55c9c09ea7e954f3360f0756e
samdaphbynet <lj8@hotmail.fr>
2023-03-20 11:06:24 +0000

    new type dog_t as a new name for the type struct dog.

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -11,12 +11,12 @@
  * @owner: of input
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	char *owner;
 	float age;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
