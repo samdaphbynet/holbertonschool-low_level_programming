ab87d579cad232ae84ecf0110e8f412860bc6f1c
samdaphbynet <lj8@hotmail.fr>
2023-03-20 09:00:17 +0000

    Define a new type struct dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Define a new type s d with the following elements:
+ * @name: name of input
+ * @age: age of input
+ * @owner: of input
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
