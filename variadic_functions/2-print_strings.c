ed300a03dfce2296833a9b3558ef36300b7aca2b
samdaphbynet <lj8@hotmail.fr>
2023-03-23 12:42:12 +0000

    function that prints string

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - function thazt prints strings,
+ * @separator: the string to be printed between the strings
+ * @n: the number of strings passed to the function
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list string;
+
+	unsigned int count;
+	char *str;
+
+	va_start(string, n);
+
+	for (count = 0; count < n; count++)
+	{
+		str = va_arg(string, char *);
+		printf("%s", str);
+
+		if (count != (n - 1) && separator != NULL)
+			printf("%s", separator);
+
+	}
+	printf("\n");
+	va_end(string);
+}
