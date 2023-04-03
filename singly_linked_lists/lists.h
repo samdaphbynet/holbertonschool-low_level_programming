a3a7f0bf1d678784bc19c9c2b406e8504a47f977
samdaphbynet <lj8@hotmail.fr>
2023-04-03 14:12:01 +0000

    added the protected from double inclusion

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,3 +1,6 @@
+#ifndef LIST_H
+#define LIST_H
+
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -22,3 +25,6 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+
+
+#endif
