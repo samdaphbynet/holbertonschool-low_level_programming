71c65c7a101e7077357f85a61003fabba73252d2
samdaphbynet <lj8@hotmail.fr>
2023-04-05 11:49:36 +0000

    update function

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -13,10 +13,16 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	temp = malloc(sizeof(dlistint_t));
 
+	if (temp == NULL)
+		return (NULL);
+
 	temp->n = n;
 	temp->next = *head;
 	temp->prev = NULL;
+	
+	if (*head != NULL)
+		(*head)->prev = temp;
 	*head = temp;
 
-	return (*head);
+	return (temp);
 }
