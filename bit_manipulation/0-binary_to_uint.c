18d8c4a99cc1d525bf56f7d3c46798e501042e95
samdaphbynet <lj8@hotmail.fr>
2023-04-07 11:22:12 +0000

    fixed the character no binary

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -13,9 +13,9 @@ unsigned int binary_to_uint(const char *b)
 
 	if (b == NULL)
 		return (0);
-	while (b[i] == '0' || b[i] == '1')
+	while (b[i] != '\0')
 	{
-		if (b[i] != '0' || b[i] != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 		binary <<= 1;
 		binary += b[i] - '0';
