6791a161d8d292d69a4802accbd019de6b4c6e68
samdaphbynet <lj8@hotmail.fr>
2023-04-07 16:27:45 +0000

    undate the function

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,9 +1,13 @@
 #include "main.h"
 
+/**
+ * print_binary - function that prints the binary representation of a number.
+ * @n: the number a converted to binary
+ */
+
 void print_binary(unsigned long int n)
 {
-	unsigned long int i;
-
-	for (i = 1UL << 31; i > 0; >>= 1)
-		printf("%c", (n & i) ? '1' : '0');
+	if (n > 1)
+		print_binary(n >> 1);
+	printf("%d", n & 1);
 }
