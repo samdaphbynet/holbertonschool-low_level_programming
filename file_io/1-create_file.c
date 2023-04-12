31c9507691e1fcb4abd935f441f6383c938592d7
samdaphbynet <lj8@hotmail.fr>
2023-04-12 10:13:26 +0000

    function that create file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * create_file - function that create file
+ * @filename: the name of file to create
+ * @text_content: the content to create in filename
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int ptr;
+
+	if (filename == NULL)
+		return (-1);
+	ptr = fopen(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR)
+		if (ptr == NULL)
+			return (-1);
+	if (text_content != NULL)
+	{
+		len = strlen(text_content);
+		new = write(ptr, text_content, len);
+
+		if (new == -1)
+		{
+			close(ptr);
+			return (-1);
+		}
+	}
+
+	close(ptr);
+	return (1);
+}
