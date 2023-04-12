6a2e1d3411bc333480192bf70aae9d8027590338
samdaphbynet <lj8@hotmail.fr>
2023-04-12 10:23:54 +0000

    change the checks of varibale ptr

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,11 +10,13 @@
 int create_file(const char *filename, char *text_content)
 {
 	int ptr;
+	size_t len;
+	ssize_t new;
 
 	if (filename == NULL)
 		return (-1);
-	ptr = fopen(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR)
-		if (ptr == NULL)
+	ptr = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+		if (ptr == -1)
 			return (-1);
 	if (text_content != NULL)
 	{
