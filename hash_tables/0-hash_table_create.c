04e9981ec2034193249551cde27aeb53a417eacf
samdaphbynet <lj8@hotmail.fr>
2023-04-13 08:13:49 +0000

    handled malloc return

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -12,8 +12,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 
 	table = (hash_table_t *)malloc(sizeof(hash_table_t));
+
+	if (table == NULL)
+		return (NULL);
+
 	table->size = size;
 	table->array = calloc(table->size, sizeof(hash_node_t *));
+
+	if (table->array == NULL)
+		return (NULL);
 
 	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
