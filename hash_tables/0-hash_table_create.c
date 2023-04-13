4ec39e298a0586f6d9cd06310ddad1007d52e797
samdaphbynet <lj8@hotmail.fr>
2023-04-13 10:10:26 +0000

    added parametre of calloc

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -17,13 +17,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	table->size = size;
-	table->array = calloc(table->size, sizeof(hash_node_t *));
+	table->array = calloc(sizeof(hash_node_t *) * size);
 
 	if (table->array == NULL)
 		return (NULL);
 
-	for (i = 0; i < table->size; i++)
+	for (i = 0; i < size; i++)
 		table->array[i] = NULL;
-	free(table);
 	return (table);
 }
