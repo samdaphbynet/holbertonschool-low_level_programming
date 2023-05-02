e6e763aa9a276ec8b5e95b5a48c55ba177c6b10d
samdaphbynet <lj8@hotmail.fr>
2023-05-03 00:53:00 +0200

    add comma in printf

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -26,7 +26,7 @@ void hash_table_print(const hash_table_t *ht)
 				first = 0;
 			else
 				printf(", ");
-			printf("'%s: '%s'", node->key, node->value);
+			printf("'%s': '%s'", node->key, node->value);
 			node = node->next;
 		}
 	}
